6cf5f8b624c4bf2a9dfd170c0836e8b851788d7c
Ferruccio Barletta <ferruccio.barletta@gmail.com>
2026-02-13 05:43:56 -0500

    1.15.2

--- Formula/g4tools.rb
+++ Formula/g4tools.rb
@@ -1,9 +1,9 @@
 class G4tools < Formula
-    version "1.15.1"
+    version "1.15.2"
     desc "G4 command-line tools"
     homepage "https://github.com/microsearch/homebrew-g4"
     url "https://github.com/microsearch/homebrew-g4/releases/download/v#{version}/g4tools-macos-aarch64-v#{version}.tar.gz"
-    sha256 "0ed6125d4dccc8a9da8c9bab6ed25153e4a70c9638fc80d6f6f3cf00e144679d"
+    sha256 "05b987e101ec29085719b93314410522a86ec412074ff887e5604e52612f2852"
 
     depends_on "deno"
 
